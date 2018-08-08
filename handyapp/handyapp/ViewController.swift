f77b233222c39029ed88143c54ff46de658502bd
Dmitriy Kuvshinnikov <schoolbox@inbox.ru>
2018-08-08 15:52:56 +0300

    Start project

---
+++ handyapp/handyapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  handyapp
+//
+//  Created by Kuvshinnikov Dmitriy on 08/08/2018.
+//  Copyright © 2018 Kuvshinnikov Dmitriy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ handyapp/handyappTests/handyappTests.swift
@@ -0,0 +1,34 @@
+//
+//  handyappTests.swift
+//  handyappTests
+//
+//  Created by Kuvshinnikov Dmitriy on 08/08/2018.
+//  Copyright © 2018 Kuvshinnikov Dmitriy. All rights reserved.
+//
+
+import XCTest
+@testable import handyapp
+
+class handyappTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
